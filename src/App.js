8ca48f75be39faf5eeed41604212948561c80765
Christophe Porteneuve <christophe@delicious-insights.com>
2021-04-10 00:02:41 +0200

    My solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
+import React from 'react'
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          right: 0,
+          height: '40%',
+          backgroundColor: 'hsl(195deg, 20%, 86%)',
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
